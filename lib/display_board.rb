08bd79d4ca83fba22af3303a407682b46648f0e4
Emmanuel J Altamirano <mannyaltamirano@yahoo.com>
2018-07-18 17:33:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 def display_board
  puts "A Tic Tac Toe Board"
- puts "   |   |     "
- puts "  ----------- "
- puts "   |   |     "
- puts "  ----------- "
- puts "   |   |     "
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
 end
  puts "A Tic Tac Toe Board"
  display_board
